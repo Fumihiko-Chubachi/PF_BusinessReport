b7c6650585f5d7e8bd775f975ca18bdc5a44406d
Fumihiko-Chubachi <aria.bravemen@gmail.com>
2022-06-11 08:28:08 +0000

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ユーザー用
+  # URL /user/sign_in ...
+  devise_for :users, controllers: {
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
 end
